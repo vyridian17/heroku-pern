13bb2ceeff915abb187414a04b4859aa0085e604
Michael Dale <vyridian@gmail.com>
2022-02-16 15:41:04 +0900

    testing app

--- index.js
+++ index.js
@@ -11,11 +11,6 @@ app.use(express.static(path.join(__dirname, './frontend/build')));
 const dbconfig = require('./knexfile')[process.env.NODE_ENV]
 const db = knex(dbconfig)
 
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "frontend/build/"));
-// });
-
-
 app.get('/api/', async (req, res) => {
   const bugs = await db.select().from('pernheroku');
   res.send(bugs);
