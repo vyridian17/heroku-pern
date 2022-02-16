2dea47184197a25dc8bd9a4b5d47945cb61373f5
Michael Dale <vyridian@gmail.com>
2022-02-16 14:01:59 +0900

    testing app

--- knexfile.js
+++ knexfile.js
@@ -15,10 +15,10 @@ module.exports = {
       database: process.env.DB_DATABASE
     },
      migrations: {
-      directory: __dirname + 'migrations'
+      directory: 'migrations'
     },
     seeds: {
-      directory: __dirname + 'seeds'
+      directory: 'seeds'
     }
   },
 
@@ -30,10 +30,10 @@ module.exports = {
       max: 10
     },
     migrations: {
-      tableName: __dirname + 'migrations'
+      tableName: 'migrations'
     },
     seeds: {
-      directory: __dirname + 'seeds'
+      directory: 'seeds'
     }
   }
 
